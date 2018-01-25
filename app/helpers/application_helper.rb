55ac25a7dbd015734a7dd2d0c7fd2fd97b781c08
Jacob Clark <clarkjac14@students.ecu.edu>
2018-01-25 15:37:30 +0000

    Added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # Returns a page title for each page of the site.
+    def full_title(page_title='')
+        base_title = "Ruby on Rails Tutorial Sample App"
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
